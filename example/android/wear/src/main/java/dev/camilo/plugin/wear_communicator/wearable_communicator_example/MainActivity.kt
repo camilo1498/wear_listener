dedfe33f6547f2923801c9b55111bcdecdd6bc7b
camilo1498 <104171332+dcamilovelandia@users.noreply.github.com>
2022-07-21 13:50:25 -0500

    thrid

--- example/android/wear/src/main/java/dev/camilo/plugin/wear_communicator/wearable_communicator_example/MainActivity.kt
+++ example/android/wear/src/main/java/dev/camilo/plugin/wear_communicator/wearable_communicator_example/MainActivity.kt
@@ -1,26 +1,39 @@
 package dev.camilo.plugin.wear_communicator.wearable_communicator_example
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.graphics.Bitmap
-import android.graphics.Point
+import android.graphics.Color
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.util.Log
-import android.view.Display
-import android.view.WindowManager
 import com.google.android.gms.wearable.*
 import com.google.zxing.BarcodeFormat
-import com.journeyapps.barcodescanner.BarcodeEncoder
+import com.google.zxing.EncodeHintType
+import com.google.zxing.qrcode.QRCodeWriter
 import dev.camilo.plugin.wear_communicator.wearable_communicator_example.databinding.ActivityMainBinding
 import kotlin.random.Random
 
 class MainActivity : Activity(),
     DataClient.OnDataChangedListener,
-    MessageClient.OnMessageReceivedListener{
+    MessageClient.OnMessageReceivedListener {
+    /** variables **/
+    private val counter = object : CountDownTimer(13000, 1000) {
+
+        override fun onTick(millisUntilFinished: Long) {
+            binding.timer.text = (millisUntilFinished / 1000).toString()
+            
+        }
+
+        override fun onFinish() {
+            this.start()
+        }
+    }.start()
 
     private lateinit var binding: ActivityMainBinding
-    private val qrCodeData: BarcodeEncoder = BarcodeEncoder()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         /** instance view **/
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -31,17 +44,20 @@ class MainActivity : Activity(),
             val messageClient = Wearable.getMessageClient(this)
             Wearable.getNodeClient(this).connectedNodes.addOnSuccessListener { nodes ->
                 nodes.forEach {
-                    messageClient.sendMessage(it.id, "/token", Random(34535345).toString().toByteArray()).addOnSuccessListener {
+                    messageClient.sendMessage(
+                        it.id,
+                        "/token",
+                        Random(34535345).toString().toByteArray()
+                    ).addOnSuccessListener {
                         Log.d("Wear", "Sent message to phone")
                     }
                 }
             }
 
         }
-
+        counter.start()
 
     }
-
 
     override fun onDataChanged(dataEvents: DataEventBuffer) {
         dataEvents.forEach { event ->
@@ -51,43 +67,26 @@ class MainActivity : Activity(),
         }
     }
 
+    private fun getQrCodeBitmap(token: String): Bitmap {
+        val size = 512 //pixels
+        val hints = hashMapOf<EncodeHintType, Int>().also {
+            it[EncodeHintType.MARGIN] = 1
+        } // Make the QR code buffer border narrower
+        val bits = QRCodeWriter().encode(token, BarcodeFormat.QR_CODE, size, size, hints)
+        return Bitmap.createBitmap(size, size, Bitmap.Config.RGB_565).also {
+            for (x in 0 until size) {
+                for (y in 0 until size) {
+                    it.setPixel(x, y, if (bits[x, y]) Color.WHITE else Color.BLACK)
+                }
+            }
+        }
+    }
+
     override fun onMessageReceived(message: MessageEvent) {
         Log.d("Weaar", String(message.data))
-        //binding.textTitle.text = String(message.data)
-
-        //below line is for getting the windowmanager service.
-        //below line is for getting the windowmanager service.
-        val manager = getSystemService(WINDOW_SERVICE) as WindowManager
-        //initializing a variable for default display.
-        //initializing a variable for default display.
-        val display: Display = manager.defaultDisplay
-        //creating a variable for point which is to be displayed in QR Code.
-        //creating a variable for point which is to be displayed in QR Code.
-        val point = Point()
-        display.getSize(point)
-        //getting width and height of a point
-        //getting width and height of a point
-        val width = point.x
-        val height = point.y
-        //generating dimension from width and height.
-        //generating dimension from width and height.
-        var dimen = if (width < height) width else height
-        dimen = dimen * 3 / 4
-        //setting this dimensions inside our qr code encoder to generate our qr code.
-        //setting this dimensions inside our qr code encoder to generate our qr code.
-
-        val bitmap: Bitmap = qrCodeData.encodeBitmap(
-            message.data.decodeToString(),
-            BarcodeFormat.QR_CODE,
-            200,
-            200,
-        )
-
 
         try {
-            //getting our qrcode in the form of bitmap.
-            // the bitmap is set inside our image view using .setimagebitmap method.
-            binding.qrImage.setImageBitmap(bitmap)
+            binding.qrImage.setImageBitmap(getQrCodeBitmap(message.data.decodeToString()))
 
         } catch (e: Exception) {
             //this method is called for exception handling.
@@ -100,13 +99,14 @@ class MainActivity : Activity(),
         super.onResume()
         Wearable.getMessageClient(this).addListener(this)
         Wearable.getDataClient(this).addListener(this)
+        counter.start()
     }
 
     override fun onDestroy() {
+        Log.e("Terminated", "app was closed")
+        counter.cancel()
         super.onDestroy()
-        Log.e("Termianted", "app was closed")
     }
-
 
 
 }
